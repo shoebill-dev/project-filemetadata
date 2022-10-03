b9438cfb31f8dddd52bdafa65f3b405b57ed3674
Johan <dev@shoebill.nl>
2022-10-03 22:22:59 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), ((req, res) => {
+  try {
+    const type = req.file.mimetype;
+    const name = req.file.originalname;
+    const size = req.file.size;
 
+
+    res.status(200).json({name, type, size});
+  } catch (error) {
+    res.status(400).send({error});
+  }
+}));
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function() {
+  console.log('Your app is listening on port ' + port);
 });
+``;
